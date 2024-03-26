8c07cecfb6afe1d828e6302aaba744a8e0cb39c8
Jake Donham <jaked@github.com>
2024-03-26 13:30:05 -0700

    custom JSON renderer using react-json-view

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig([
   {
-    entry: ['./src/extension.ts'],
+    entry: ['./src/extension.ts', './src/notebook/objectRenderer.tsx'],
     external: ['vscode'],
-    format: 'cjs',
+    format: ['cjs', 'esm'],
+    splitting: false,
+    esbuildOptions(options) {
+      options.define = {
+        'process.env.NODE_ENV': 'production',
+      }
+    },
   },
 ])
